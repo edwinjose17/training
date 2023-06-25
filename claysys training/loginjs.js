ca74cc1267bd3d69c14455b7c9c754dab1a06b8d
edwinjose17 <60867822+edwinjose17@users.noreply.github.com>
2023-06-25 23:07:49 +0530

    Add files via upload

---
+++ claysys training/loginjs.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var loginButton = document.getElementById("loginButton");
+  loginButton.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Perform validation
+    if (username === "" || password === "") {
+      alert("Please enter both username and password.");
+      return;
+    }
+
+    // Validation passed
+    alert("Welcome, " + username + "! You are now logged in.");
+
+    // You can perform further actions, such as submitting the form or redirecting to another page.
+    window.location.href = "finalpage.html";
+  });
+});
